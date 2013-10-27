880854a70773bd3693b5dd9b24dc32a08274c863
PeeHaa <info@pieterhordijk.com>
2013-10-27 18:12:41 +0100

    Added exceptions docblocks

--- src/Requestable/Network/Client/CurlException.php
+++ src/Requestable/Network/Client/CurlException.php
@@ -1,7 +1,27 @@
 <?php
-
+/**
+ * cURL HTTP client exception class
+ *
+ * PHP version 5.4
+ *
+ * @category   Requestable
+ * @package    Network
+ * @subpackage Client
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
 namespace Requestable\Network\Client;
 
+/**
+ * cURL HTTP client exception class
+ *
+ * @category   Requestable
+ * @package    Network
+ * @subpackage Client
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
 class CurlException extends Exception
 {
 }
