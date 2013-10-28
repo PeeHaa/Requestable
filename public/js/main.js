4ef99733884a74a31d4961bda3272386641226c9
PeeHaa <info@pieterhordijk.com>
2013-10-28 20:44:03 +0100

    Cleaned up JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,7 @@
 (function() {
-    var animator, total,
-        result = document.querySelector('.result'),
-        durationMS = 1000;
-
-    if (result) {
-        total = result.offsetTop;
-        animator = new Animator(result);
+    function scrollTheThing(result) {
+        var total    = result.offsetTop,
+            animator = new Animator(result);
 
         animator.registerEasingFunction("bounce", function(pos) {
             if ((pos) < (1/2.75)) {
@@ -22,11 +18,63 @@
         animator.animate({
             startValue: window.scrollY,
             endValue: total,
-            totalTime: durationMS,
+            totalTime: 1000,
             frameFunc: function(newValue) {
                 window.scroll(window.scrollX, newValue);
             },
             easing: "bounce"
         });
     }
+
+    // this will make @daverandom (and every other sane person) cry
+    function stringToHtml(string) {
+        var container = document.createElement('div');
+        container.innerHTML = string;
+
+        return container.childNodes;
+    }
+
+    var result = document.querySelector('.result');
+
+    if (result) {
+        scrollTheThing(result);
+    }
+
+    var form = document.querySelector('form');
+    $(form).on('submit', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState < 4 || xhr.status !== 200) {
+                return;
+            }
+
+            if (xhr.readyState === 4) {
+                var data = JSON.parse(xhr.responseText);
+
+                var results = document.querySelectorAll('.result');
+                for (var i =0, l = results.length; i < l; i++) {
+                    results[i].parentNode.removeChild(results[i]);
+                }
+
+                var content = document.querySelector('#body .content');
+                var newResults = stringToHtml(data.result);
+                for (var i = 0, l = newResults.length; i < l; i++) {
+                    if (typeof newResults[i] === 'undefined' || newResults[i].nodeType === 3) {
+                        continue;
+                    }
+
+                    content.appendChild(newResults[i]);
+                }
+
+                scrollTheThing(document.querySelector('.result'));
+            }
+        }
+        xhr.open(form.getAttribute('method'), '/', true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.send($(form).serialize());
+    });
 }());
