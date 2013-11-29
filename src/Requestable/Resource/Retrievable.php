5c6e5479cd1266fcdb9d7fdaf54181a594ed1abf
PeeHaa <info@pieterhordijk.com>
2013-11-29 01:56:42 +0100

    Added request retriever

---
+++ src/Requestable/Resource/Retrievable.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Interface for classes that retrieve requests
+ *
+ * PHP version 5.4
+ *
+ * @category   Requestable
+ * @package    Resource
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Requestable\Resource;
+
+/**
+ * Interface for classes that retrieve requests
+ *
+ * @category   Requestable
+ * @package    Resource
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Retrievable
+{
+    /**
+     * Gets the request from the storage
+     *
+     * @return \Requestable\Data\Storage The request
+     */
+    public function getRequest();
+}
---
+++ src/Requestable/Resource/Retriever.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Retrieves requests
+ *
+ * PHP version 5.4
+ *
+ * @category   Requestable
+ * @package    Resource
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Requestable\Resource;
+
+use Requestable\Data\Storage;
+
+/**
+ * Retrieves requests
+ *
+ * @category   Requestable
+ * @package    Resource
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Retriever implements Retrievable
+{
+    /**
+     * @var int The id of the request
+     */
+    private $id;
+
+    /**
+     * @var \PDO The database connection
+     */
+    private $dbConnection;
+
+    /**
+     * Creates instance
+     *
+     * @param int  $id           The id of the request
+     * @param \PDO $dbConnection The database connection
+     */
+    public function __construct($id, \PDO $dbConnection)
+    {
+        $this->id           = $id;
+        $this->dbConnection = $dbConnection;
+    }
+
+    /**
+     * Gets the request from the storage
+     *
+     * @return \Requestable\Data\Storage The request
+     */
+    public function getRequest()
+    {
+        $query = 'SELECT requests.id, requests.uri, requests.method, requests.follow, requests.cookies, requests.body,';
+        $query.= ' requestheaders.header';
+        $query.= ' FROM requests';
+        $query.= ' LEFT JOIN requestheaders ON requestheaders.requestid = requests.id';
+        $query.= ' WHERE requests.id = :id';
+        $query.= ' ORDER BY requestheaders.id';
+
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->execute([
+            'id' => $this->id,
+        ]);
+
+        return new Storage($stmt->fetchAll());
+    }
+}
